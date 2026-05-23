a3b328de62db652eba9b068768f4ced9916ab13e
Kirakiraz <hugoprhg@gmail.com>
2026-05-24 03:43:16 +0700

    git commit -m "refactor: secure database credentials using python-dotenv"

--- currency_api.py
+++ currency_api.py
@@ -4,7 +4,7 @@ import pandas as pd
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text, types
 
-load_dotenv
+load_dotenv()
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
